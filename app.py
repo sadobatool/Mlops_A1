0d13d00f7f7edeb1a83f8d0d06fd0935462da136
saimsaleem <engssaleem@gmail.com>
2024-03-30 20:12:39 +0500

    f

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix 
 
 data = pd.read_csv('heart.csv')
 
