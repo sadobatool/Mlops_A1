f69a3efa77048881450827d79bcaba1c4d8a00f3
saimsaleem <engssaleem@gmail.com>
2024-03-30 20:42:47 +0500

    Trigger Merge

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ data = pd.read_csv('heart.csv')
 X = data.drop('target', axis=1)
 y = data['target']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) 
 
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
