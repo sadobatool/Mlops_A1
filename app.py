d9182e1c34b34de838b6042e99a78de49347ffb0
saimsaleem <engssaleem@gmail.com>
2024-03-30 20:01:52 +0500

    Push

--- app.py
+++ app.py
@@ -5,29 +5,22 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
-# Load the dataset
-data = pd.read_csv('heart.csv')  # Make sure the file name matches
+data = pd.read_csv('heart.csv')
 
-# Prepare the data
-X = data.drop('target', axis=1)  # Features
-y = data['target']  # Target variable
+X = data.drop('target', axis=1)
+y = data['target']
 
-# dataset split into training and testing sets 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Scale the features
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-# Train the logistic regression model
 model = LogisticRegression(random_state=42)
 model.fit(X_train_scaled, y_train)
 
-# Make predictions on the test set
 y_pred = model.predict(X_test_scaled)
 
-# Evaluate the model
 accuracy = accuracy_score(y_test, y_pred)
 print(f'Accuracy: {accuracy*100:.2f}%')
 print(classification_report(y_test, y_pred))
